68587bb400bd395c915c251836d32ab5a13eb571
roxanne <oxana.tish@gmail.com>
2019-07-05 17:56:13 +0300

    add tasks

--- list/app.js
+++ list/app.js
@@ -15,5 +15,23 @@ function addTask(e) {
     alert('Add a task');
   }
 
+  const li = document.createElement('li');
+
+  li.className = 'collection-item';
+
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  const link = document.createElement('a');
+
+  link.className = 'delete-item secondary-content';
+
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+
+  li.appendChild(link);
+
+  taskList.appendChild(li);
+
+  taskInput.value = '';
+
   e.preventDefault();
 }
