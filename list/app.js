5c6fd7c0d2f3c20756ec99559cd8fabaf1d45ce4
roxanne <oxana.tish@gmail.com>
2019-07-05 17:08:38 +0300

    const added

---
+++ list/app.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+loadEventListeners();
+
+function loadEventListeners() {
+  form.addEventListener('submit', addTask);
+}
+
+function addTask(e) {
+  if(taskInput.value === ''){
+    alert('Add a task');
+  }
+
+  e.preventDefault();
+}
